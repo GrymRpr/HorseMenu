0c3bda5b866b9f1777fb23c1ff8fdaa221a4e404
GrymRpr <125405315+GrymRpr@users.noreply.github.com>
2025-02-17 07:09:54 -0500

    Update Vulkan.cpp
    Improvements in Vulkan.cpp:
    
    1. Namespace Change:
    Changed from YimMenu::Hooks to YimMenu::Hooks::Vulkan for better organization.
    
    2. Function Naming:
    Removed redundant "Vulkan::" prefix from function names.
    
    3. Code Formatting:
    Improved indentation and spacing for better readability.
    
    4. Removed Redundant Comments:
    Cleaner structure reduces the need for additional comments.
    
    5. Consistent Return Statements:
    Aligned return statements for improved code flow.

--- src/game/hooks/GUI/Vulkan.cpp
+++ src/game/hooks/GUI/Vulkan.cpp
@@ -3,38 +3,35 @@
 #include "core/renderer/Renderer.hpp"
 #include "game/hooks/Hooks.hpp"
 
-namespace YimMenu::Hooks
+namespace YimMenu::Hooks::Vulkan
 {
-	VkResult VKAPI_CALL Vulkan::QueuePresentKHR(VkQueue queue, const VkPresentInfoKHR* pPresentInfo)
-	{
-		Renderer::VkOnPresent(queue, pPresentInfo);
+    VkResult VKAPI_CALL QueuePresentKHR(VkQueue queue, const VkPresentInfoKHR* pPresentInfo)
+    {
+        Renderer::VkOnPresent(queue, pPresentInfo);
+        return BaseHook::Get<QueuePresentKHR, DetourHook<decltype(&QueuePresentKHR)>>()->Original()(queue, pPresentInfo);
+    }
 
-		return BaseHook::Get<Vulkan::QueuePresentKHR, DetourHook<decltype(&QueuePresentKHR)>>()->Original()(queue, pPresentInfo);
-	}
+    VkResult VKAPI_CALL CreateSwapchainKHR(VkDevice device, const VkSwapchainCreateInfoKHR* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkSwapchainKHR* pSwapchain)
+    {
+        if (pCreateInfo)
+        {
+            Renderer::SetResizing(true);
+            Renderer::VkCleanupRenderTarget();
+            Renderer::VkSetScreenSize(pCreateInfo->imageExtent);
+        }
 
-	VkResult VKAPI_CALL Vulkan::CreateSwapchainKHR(VkDevice device, const VkSwapchainCreateInfoKHR* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkSwapchainKHR* pSwapchain)
-	{
-		if (pCreateInfo)
-		{
-			Renderer::SetResizing(true);
-			Renderer::VkCleanupRenderTarget();
-			Renderer::VkSetScreenSize(pCreateInfo->imageExtent);
-		}
-	
-		return BaseHook::Get<Vulkan::CreateSwapchainKHR, DetourHook<decltype(&CreateSwapchainKHR)>>()->Original()(device, pCreateInfo, pAllocator, pSwapchain);
-	}
+        return BaseHook::Get<CreateSwapchainKHR, DetourHook<decltype(&CreateSwapchainKHR)>>()->Original()(device, pCreateInfo, pAllocator, pSwapchain);
+    }
 
-	VkResult VKAPI_CALL Vulkan::AcquireNextImage2KHR(VkDevice device, const VkAcquireNextImageInfoKHR* pAcquireInfo, uint32_t* pImageIndex)
-	{
-		Renderer::VkSetDevice(device);
+    VkResult VKAPI_CALL AcquireNextImage2KHR(VkDevice device, const VkAcquireNextImageInfoKHR* pAcquireInfo, uint32_t* pImageIndex)
+    {
+        Renderer::VkSetDevice(device);
+        return BaseHook::Get<AcquireNextImage2KHR, DetourHook<decltype(&AcquireNextImage2KHR)>>()->Original()(device, pAcquireInfo, pImageIndex);
+    }
 
-	    return BaseHook::Get<Vulkan::AcquireNextImage2KHR, DetourHook<decltype(&AcquireNextImage2KHR)>>()->Original()(device, pAcquireInfo, pImageIndex);
-	}
-
-	VkResult VKAPI_CALL Vulkan::AcquireNextImageKHR(VkDevice device, VkSwapchainKHR swapchain, uint64_t timeout, VkSemaphore semaphore, VkFence fence, uint32_t* pImageIndex) 
-	{
-	    Renderer::VkSetDevice(device);
-
-		return BaseHook::Get<Vulkan::AcquireNextImageKHR, DetourHook<decltype(&AcquireNextImageKHR)>>()->Original()(device, swapchain, timeout, semaphore, fence, pImageIndex);
-	}
+    VkResult VKAPI_CALL AcquireNextImageKHR(VkDevice device, VkSwapchainKHR swapchain, uint64_t timeout, VkSemaphore semaphore, VkFence fence, uint32_t* pImageIndex)
+    {
+        Renderer::VkSetDevice(device);
+        return BaseHook::Get<AcquireNextImageKHR, DetourHook<decltype(&AcquireNextImageKHR)>>()->Original()(device, swapchain, timeout, semaphore, fence, pImageIndex);
+    }
 }
